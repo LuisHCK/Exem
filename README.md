f0337f3f6e0ffe1be202ba8352e3a57e18e7ac94
Kirk Douglas Jr <kirkd@uniregistry.com>
2017-09-04 13:22:24 -0500

    init

---
+++ README.md
@@ -0,0 +1,106 @@
+Expo.io Exem SQLite Orm
+===================
+
+Expo.io SQLite/Webql Orm.
+
+I made a very simple orm just made for getting data easier than how Expo's SQLite modules is. With hopes of getting help from other developers to make this very and useful for future developers.
+
+> **Disclaimer**: I'm a great fan of Expo.io but there is a lot of downfalls to to. So instead of complaining i decided to help. 
+
+### How to use:
+
+First you have to import it (duuh?).
+```
+var Exem = require("Exem")("data.db")
+```
+
+Now that we have Exem we can create models,  we will create a `Customer` model that we will use to work with our `customer` table.
+```
+var Store = db.table("store")
+```
+Not much logic goes into these for now. I hope for the future relationships could be added.
+
+### Wait... What table?
+Oh yeah we forgot to make a table. Now i made this for a simple project I was working on, so it's missing a few things. But ima just throw this in here.
+
+```
+Store.schema((table) => {
+    return {
+        id: table.int({primary_key: true}),
+        years_active: table.int(),
+        name: table.string(), // TEXT TYPE
+        contact_number: table.string(),
+        contact_email: table.string(),
+        address: table.string(),
+        created: table.datetime()
+    }
+}).create()
+```
+
+This will create the table if it doesn't exist.
+
+### Querying
+
+Here are a few examples how to query data.
+
+#### Select
+```
+Store.select({name: "Payless", years_active: 10})
+// SELECT * FROM store WHERE name = 'Payless' AND years_active = 10
+```
+```
+Store.all()
+// SELECT * FROM store
+```
+
+#### Search
+```
+Store.select({name: "less", address: "Thomas Stre"}
+// SELECT * FROM store WHERE name like '%less%' OR address LIKE '%Thomas Stre%'
+```
+
+#### Search
+```
+Store.select({name: "less", address: "Thomas Stre"}
+// SELECT * FROM store WHERE name like '%less%' OR address LIKE '%Thomas Stre%'
+```
+
+#### Update
+To update you must include an id property so Exem will know which field to update, it will take the id out and use the rest of the data to update the record.
+```
+Store.update({id: 1, address: "#25 Jackson Lane, Konaha"}
+// UPDATE store SET address = '#25 Jackson Lane, Konaha' WHERE id = 1
+```
+
+#### Delete
+Delete only needs the id property to know what to delete. you can pass in other properties if you have an Object that already has the id in it, it will just ignore the rest...
+```
+Store.delete({id: 1, address: "#25 Jackson Lane, Konaha"}
+// DELETE FROM store WHERE id = 1
+```
+
+### Results
+
+So you know that Expo's SQLite modules uses events.... well we uses promises, why? Because it will eventually happen...
+
+So a successful promise will return's a ResultSet object from Expo's module. You can read (a little) more about it here: https://docs.expo.io/versions/latest/sdk/sqlite.html#resultset
+
+```
+Store.select({id: 5}).then((result) => {
+	let store = result.rows.item(0)
+	if (store) {
+		console.log("Found Store", store))
+	} else {
+		throw "Couldn't find store"
+	}		
+}).catch((error) => {
+	// Expo's module may also throw an exception as well
+	console.warn("Error occurred", error) 
+})
+```
+
+Well there you have it. I hope this that be improved until it's like Expo's own version of Knex or something.
+
+I want to mention I've seen drivers that we can use to use Knex and Expo's module but for what?? I feel like Expo's goal is to make things easier for us. And that's what I want this package to be...
+
+Thank you, feel free to submit your pull requests, i'll review them as soon as I can.
---
+++ index.js
@@ -0,0 +1,139 @@
+var Promise = require("bluebird")
+var SQLite = require("expo").SQLite
+
+var Exem = (sqlite_file) => {
+    let db = SQLite.openDatabase(sqlite_file);
+    return new Database(db)
+}
+
+class Database {
+    constructor(db) {
+        this.db = db
+    }
+    table(name) {
+        return new Table(this.db, name)
+    }
+    
+    exec(query, params) {
+        return new Table(this.db, null, null).execute_sql(query, params)
+    }
+}
+
+class Table {
+    constructor(db, name) {
+        this.db = db
+        this.name = name
+    }
+
+    schema(table_schema) {
+        let schema = table_schema(new TableValueTypes())
+        let columns = Object.entries(schema).map((c) => { return c[1].replace("{column}", c[0]) }).join(", ")
+        let create_table_query = "create table if not exists " + this.name  +" (" + columns + ");"
+        return {
+            "create": this.execute_sql.bind(this, create_table_query),
+            "schema": create_table_query
+        }
+    }
+
+    // create table if not exists items (id integer primary key not null, done int, value text);
+
+    execute_sql(query, params) {
+        params = params || []
+
+        console.log("Executed query:", query, params)
+
+        exec = (resolve, reject) => {
+            let resolve_func = (tx, results) => { return resolve(results) }
+            let reject_func = (tx, results) => { return reject(results) }
+            this.db.transaction(tx => {
+                tx.executeSql(query, params, resolve_func, reject_func)
+            })
+        }
+        return new Promise(exec.bind(this));
+    }
+
+    select(params) {
+        let where_clause = Object.entries(params).map((d) => { 
+            let column = d[0]
+            return column + " = ?"
+        }).join(" AND ")
+
+        let values = Object.values(params)
+
+        let query = "select * from " + this.name + " where " + where_clause + ";"
+
+        return this.execute_sql(query, values)
+    }
+
+    search(params) {
+        let where_clause = Object.entries(params).map((d) => { 
+            let column = d[0]
+            return column + " like ?"
+        }).join(" OR ")
+
+        let values = Object.values(params).map((val) => { return "%" + val + "%" })
+
+        let query = "select * from " + this.name + " where " + where_clause + ";"
+        return this.execute_sql(query, values)
+    }
+
+    all() {
+        let query = "select * from " + this.name + ";"
+        return this.execute_sql(query, [])
+    }
+
+    insert(params) {
+        var columns = Object.keys(params).join(", ")
+        var values = Object.values(params)
+        var values_placeholders = Object.values(params).map(c => { return "?" }).join(", ")
+        let query = "insert into " + this.name + " (" + columns +")  values (" + values_placeholders + ");"
+        return this.execute_sql(query, values)
+    }
+
+    update(params) {
+        // get id
+        if (!params.id)
+            throw "PropertyError: `id` required to perfrom update"
+        let table_id = params.id
+        delete params.id
+
+        let set_clause = Object.entries(params).map((d) => { 
+            let column = d[0]
+            return column + " = ?"
+        }).join(", ")
+        var values = Object.values(params)
+        values.push(table_id)
+        
+        let query = "update " + this.name + " set " + set_clause + " where id = ?;"
+        return this.execute_sql(query, values)
+    }
+
+    delete(params) {
+        // get id
+        if (!params.id)
+            throw "PropertyError: `id` required to perfrom update"
+        let table_id = params.id
+        let query = "delete from " + this.name + " where id = ?;"
+        return this.execute_sql(query, [table_id])
+    }
+}
+
+class TableValueTypes {
+
+    string() {
+        return "{column} text"
+    }
+
+    datetime() {
+        return "{column} datetime"
+    }
+
+    int(data) {
+        data = {auto_increment: false, primary_key: false, ...data}
+        return "{column} integer {primary_key}"
+        .replace("{length}", data.length)
+        .replace("{primary_key}", data.primary_key ? "primary key" : "")
+    }
+}
+
+module.exports = Exem
